3f38d28f3302f8cded0957f77f3b4479b859c66e
KROSSEYE <30990184+KROSSEYE@users.noreply.github.com>
2023-08-26 17:14:58 +0100

    improve icon dictionary layout and add formats

--- file_icons.py
+++ file_icons.py
@@ -1,4 +1,5 @@
 ICON_MAP = {
+    # Image Formats
     ".jpg": "🖼️",      # JPEG image
     ".jpeg": "🖼️",
     ".png": "🖼️",
@@ -9,17 +10,28 @@ ICON_MAP = {
     ".ico": "🖼️",      # Icon file
     ".tif": "🖼️",      # TIFF image
     ".tiff": "🖼️",
+    ".jp2": "🖼️",      # JPEG 2000 image
+    ".jxr": "🖼️",      # JPEG XR image
+
+    # Audio Formats
     ".mp3": "🎵",       # MP3 audio
     ".wav": "🎵",
     ".ogg": "🎵",
     ".flac": "🎵",      # FLAC audio
     ".aac": "🎵",       # AAC audio
     ".wma": "🎵",       # WMA audio
+    ".m4a": "🎵",       # M4A audio
+
+    # Video Formats
     ".mp4": "📹",       # MP4 video
     ".avi": "📹",
     ".mkv": "📹",
     ".mov": "📹",       # QuickTime video
     ".wmv": "📹",       # WMV video
+    ".flv": "📹",       # Flash video
+    ".webm": "📹",      # WebM video
+
+    # Document Formats
     ".txt": "📝",       # Text file
     ".rtf": "📝",       # Rich Text Format
     ".md": "📝",        # Markdown file
@@ -27,43 +39,99 @@ ICON_MAP = {
     ".doc": "📑",       # DOC document
     ".docx": "📑",
     ".odt": "📑",       # OpenDocument Text
+    ".odp": "📊",       # OpenDocument Presentation
+    ".ods": "📈",       # OpenDocument Spreadsheet
     ".ppt": "📊",       # PowerPoint presentation
     ".pptx": "📊",
     ".xls": "📈",       # Excel spreadsheet
     ".xlsx": "📈",
+
+    # Data Formats
     ".csv": "📊",       # CSV file
+    ".json": "📊",      # JSON file
+    ".xml": "📜",       # XML file
+
+    # Archive Formats
     ".zip": "🗃️",      # Zip archive
     ".rar": "🗃️",      # RAR archive
     ".7z": "🗃️",       # 7-Zip archive
     ".tar": "🗃️",      # Tarball archive
     ".gz": "🗃️",       # Gzip archive
+    ".bz2": "🗃️",      # Bzip2 archive
+    ".xz": "🗃️",       # XZ archive
+
+    # Executable Formats
     ".exe": "💿",       # Executable file
     ".msi": "💿",       # Windows Installer package
-    ".bat": "💿",       # Batch script
-    ".cmd": "💿",       # Command script
-    ".sh": "💿",        # Shell script
     ".bin": "💿",       # Binary executable
     ".out": "💿",       # Compiled output
     ".run": "💿",       # Runnable file
     ".app": "📱",       # Application file
-    ".dmg": "💾",       # macOS disk image
     ".apk": "📱",       # Android package
     ".ipa": "📱",       # iOS app
-    ".jar": "💿",       # Java Archive
-    ".deb": "💾",       # Debian package
-    ".rpm": "💾",       # RPM package
-    ".command": "💿",   # Command script
+    ".jar": "📦",       # Java Archive
+    ".deb": "📦",       # Debian package
+    ".rpm": "📦",       # RPM package
     ".dll": "💾",       # DLL file
-    ".iso": "💿",       # DLL file
+
+    # Programming and Script Formats
+    ".bat": "📜",       # Batch script
+    ".cmd": "📜",       # Command script
+    ".sh": "📜",        # Shell script
+    ".command": "📜",   # Command script
+    ".py": "🐍",        # Python script
+    ".cpp": "💻",       # C++ source code
+    ".java": "☕",      # Java source code
+    ".js": "📜",        # JavaScript source code
+    ".html": "📄",      # HTML file
+    ".css": "🎨",       # CSS file
+    ".rb": "💎",        # Ruby script
+    ".php": "🐘",       # PHP script
+    ".swift": "🍏",     # Swift script
+    ".pl": "🐪",        # Perl script
+    ".lua": "🌙",       # Lua script
+    ".ps1": "📜",       # PowerShell script
+    ".vb": "⌨️",         # Visual Basic script
+    ".ts": "📜",        # TypeScript file
+    ".kotlin": "☕",    # Kotlin Source Code
+    ".go": "🐹",        # Go Source Code
+    ".rs": "🦀",        # Rust Source Code
+    ".cs": "#️⃣",        # C# Source Code
+    ".c": "🅒",          # C# Source Code
+
+    # Disk Image Formats
+    ".dmg": "💾",       # macOS disk image
+    ".iso": "💿",       # ISO disk image
     ".img": "💿",      # Generic disk image
     ".vhd": "🗃️",      # Virtual Hard Disk (VHD)
     ".vhdx": "🗃️",     # Virtual Hard Disk (VHDX)
     ".vmdk": "🗃️",     # Virtual Machine Disk (VMDK)
-    ".raw": "🗃️",      # Raw disk image
-    ".qcow": "🗃️",     # QEMU Copy On Write (QCOW)
-    ".qcow2": "🗃️",    # QEMU Copy On Write (QCOW2)
-    ".udf": "📀",       # Universal Disk Format (UDF)
     ".cue": "📀",       # CUE file
-    ".nrg": "📀",       # Nero Disk Image (NRG)
-    ".ape": "📀",       # Monkey's Audio Format (APE)
+
+    # Vector and Graphic Design Formats
+    ".ai": "🖼️",       # Adobe Illustrator
+    ".eps": "🖼️",      # Encapsulated PostScript
+    ".psd": "🖼️",      # Adobe Photoshop
+    ".xcf": "🖼️",      # GIMP image
+
+    # 3D and Design Formats
+    ".obj": "🏗️",      # Waveform Object 3D file
+    ".stl": "🏗️",      # Stereolithography 3D model
+    ".3ds": "🏗️",      # 3DS Max 3D model
+    ".fbx": "🏗️",      # Autodesk FBX 3D model
+    ".blend": "🏗️",    # Blender 3D model
+    ".c4d": "🏗️",       # Cinema 4D File Format
+    ".max": "🏗️",       # 3ds Max Scene File Format
+
+    # Font Formats
+    ".ttf": "🔡",       # TrueType font
+    ".otf": "🔡",       # OpenType font
+
+    # Miscellaneous Formats
+    ".torrent": "🔗",   # Torrent file
+    ".cfg": "⚙️",       # Configuration file
+    ".config": "⚙️",       # Configuration file
+    ".ini": "⚙️",       # INI configuration file
+    ".url": "🔗",       # URL shortcut
+    ".lnk": "🔗",       # Windows shortcut
 }
