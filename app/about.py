7f0d20e169c98aaa7bfb547dfb1427577b2d0938
KROSSEYE <30990184+KROSSEYE@users.noreply.github.com>
2023-08-31 11:22:43 +0100

    improve layout

--- app/about.py
+++ app/about.py
@@ -1,11 +1,17 @@
 app_version = "0.2.0 (Alpha Pre-release)"
 
-licenses = [
-        {"name": "Noto Color Emoji",
-         "version": "Unicode 15.0",
-         "license": "SIL Open Font License, version 1.1"},
+licenses = {
+    "assets": [
+        {
+            "name": "Noto Color Emoji",
+            "version": "Unicode 15.0",
+            "license": "SIL Open Font License, version 1.1"
+        },
 
-        {"name": "Inter Font",
-         "version": "v3.19",
-         "license": "SIL Open Font License, version 1.1"}
-        ]
+        {
+            "name": "Inter Font",
+            "version": "v3.19",
+            "license": "SIL Open Font License, version 1.1"
+        }
+    ]
+}
