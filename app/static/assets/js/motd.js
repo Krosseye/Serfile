fe90049cb356fb6c8cf291a0993d1df64d6fbe65
KROSSEYE <30990184+KROSSEYE@users.noreply.github.com>
2023-09-08 17:15:43 +0100

    motd results persist during session

--- app/static/assets/js/motd.js
+++ app/static/assets/js/motd.js
@@ -1,35 +1,39 @@
-function isMotdBannerClosed() {
-  return sessionStorage.getItem("motdClosed") === "true";
-}
-
-function setMotdBannerClosed() {
-  sessionStorage.setItem("motdClosed", "true");
-}
-
-// Fetch MOTD data from API
+// Fetch MOTD data from API and store in session storage
 function fetchMotdData() {
-  // Check if MOTD banner is closed in session storage
   if (isMotdBannerClosed()) {
     return;
   }
 
-  fetch("/api/motd")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      const isMotdEnabled = data.enabled;
-      const title = data.title;
-      const message = data.message;
+  if (!motdFetched) {
+    fetch("/api/motd")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const isMotdEnabled = data.enabled;
+        const title = data.title;
+        const message = data.message;
 
-      updateMotdDisplay(isMotdEnabled, title, message);
-    })
-    .catch((error) => {
-      console.error("Fetch error:", error);
-    });
+        // Store MOTD in session storage
+        sessionStorage.setItem("motdFetched", "true");
+        sessionStorage.setItem("motdTitle", title);
+        sessionStorage.setItem("motdMessage", message);
+
+        updateMotdDisplay(isMotdEnabled, title, message);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  } else {
+    // MOTD has already been fetched
+    const title = sessionStorage.getItem("motdTitle");
+    const message = sessionStorage.getItem("motdMessage");
+    updateMotdDisplay(true, title, message);
+    shouldShowMotd();
+  }
 }
 
 // Update MOTD display
@@ -49,16 +53,35 @@ function updateMotdDisplay(isMotdEnabled, title, message) {
   }
 }
 
-// Close MOTD banner and set session storage
+function isMotdBannerClosed() {
+  return sessionStorage.getItem("motdClosed") === "true";
+}
+
+function setMotdBannerClosed() {
+  sessionStorage.setItem("motdClosed", "true");
+}
+
+function shouldShowMotd() {
+  if (!motdFetched || isMotdBannerClosed()) {
+    return;
+  }
+
+  const motdBanner = document.getElementById("motd-banner");
+  if (motdBanner.style.display !== "block") {
+    showMotd(motdBanner);
+  }
+}
+
 function closeMotd(motdBanner) {
   motdBanner.style.display = "none";
   setMotdBannerClosed();
 }
 
-// Show MOTD banner
 function showMotd(motdBanner) {
   motdBanner.style.display = "block";
 }
+
+const motdFetched = sessionStorage.getItem("motdFetched") === "true";
 
 // Event listener for close button
 const closeMotdButton = document.getElementById("close-motd");
